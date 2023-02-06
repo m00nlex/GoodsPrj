fe172b41561df300b9a816577940ac0a2862645d
m00nlex <you@example.com>
2023-02-06 14:13:32 +0500

    express test

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const express = require('express');
-const pg = require('pg');
-
-const {Router} = require('express');
-const router = Router();
-
+// const pg = require('pg');
 
 const PORT = process.env.PORT || 3000;
 
-const app = express();
+// const {Router} = require('express');
+// const router = Router();
 
+
+
+const app = express();
 
 app.get('/', (req, res) => {
     res.send('Hello Aliya!');
 })
+
 app.listen(PORT, () => {
-    console.log("..SERVER STARTED!..")
+    console.log(`..SERVER STARTED on port ${PORT}!..`)
 })
