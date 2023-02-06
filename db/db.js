d09ec8bad040fd67106318dac63fe6ae1bcd2eb8
m00nlex <you@example.com>
2023-02-06 17:46:31 +0500

    db setting

---
+++ db/db.js
@@ -0,0 +1,10 @@
+const Pool = require('pg').Pool;
+const pool = new Pool({
+    user: 'postgres',
+    password: 'm00nlex',
+    host: 'localhost',
+    port: 5432,
+    database: 'TAGS'
+});
+
+module.exports = pool();
