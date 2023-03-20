67b237ccf2812f50cf3da402de52651be01c40b7
m00nlex <you@example.com>
2023-03-20 23:21:31 +0500

    changed into server and client part

--- Server/index.js
+++ Server/index.js
@@ -2,12 +2,17 @@ const express = require('express');
 const pg = require('pg');
 const config = require('config');
 const userRouter = require('./routes/user.routes')
-
+const path = require('path');
 const PORT = config.get('server.PORT');
 
 const app = express();
+
 app.use(express.json())
 app.use('/api', userRouter)
+app.use('/css', express.static(__dirname + 'public/css'))
+app.get('/',(req,res) => {
+    res.render('index', { text: 'Hey' })
+})
 
 app.listen(PORT, () => {
     console.log(`..SERVER STARTED on port ${PORT}!..`)
