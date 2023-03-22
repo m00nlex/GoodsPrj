59a7fc2d2b34e0a789d0bd79d65cf2a9a31df828
m00nlex <you@example.com>
2023-03-23 00:34:32 +0500

    uuid + filedupload added

--- Server/index.js
+++ Server/index.js
@@ -6,13 +6,17 @@ const sequelize = require('./db/db.js')
 const path = require('path');
 const models = require('./models/models.js')
 const cors = require('cors')
+const errorHandler = require('./middleware/ErrorHandlingMiddleware.js')
+const fileUpload = require('express-fileupload')
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors())
 app.use(express.json()) //чтобы приложение могло парсить json
+app.use(fileUpload({}))
 app.use('/api', index_router)
+app.use(errorHandler) //хэндлеры с ошибками всегда в самом конце инициализации!
 const start = async () => {
     try {
         await sequelize.authenticate()
