84d34a57255bff07c74e5766876e10268a4cc807
m00nlex <you@example.com>
2023-03-22 20:54:44 +0500

    uncapable to use .env somehow. need'da fix

--- Server/db/db.js
+++ Server/db/db.js
@@ -1,9 +1,9 @@
 const {Sequelize} = require('sequelize')
 
 module.exports = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    'TAGST', //DB NAME
+    'postgres', //USERNAME
+    'm00nlex', //PASS
     {
         dialect: 'postgres',
         host: process.env.DB_HOST,
