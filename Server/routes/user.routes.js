7b73e14cfcea03c2056dfc84df9437052548202a
m00nlex <you@example.com>
2023-03-23 00:36:39 +0500

    + log / reg / auth demo

--- Server/routes/user.routes.js
+++ Server/routes/user.routes.js
@@ -5,5 +5,8 @@ const userController = require('../controller/user.controller.js')
 
 router.get('/getusers', userController.getUsers)
 router.get('/getuser/:id', userController.getUser)
+router.post('/login', userController.login)
+router.post('/registration', userController.registration)
+router.get('/auth', userController.check)
 
 module.exports = router;
