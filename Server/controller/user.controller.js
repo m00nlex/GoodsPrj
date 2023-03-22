5955d0f6007159474a386b227d30b224c7595ff6
m00nlex <you@example.com>
2023-03-23 00:36:18 +0500

    req check +

--- Server/controller/user.controller.js
+++ Server/controller/user.controller.js
@@ -1,20 +1,30 @@
 const db = require('../db/db.js')
-
+const ApiError = require('../err/ApiError.js')
 class UserController{
     async getUsers(req, res){
         const users = await db.query(`SELECT * FROM users`)
-        if (users.rows){
-            res.json(users.rows)
-        } else {
-            res.json('nothing')
-        }
+        res.json(users.rows ? users.rows : "nothing is here")
+
     }
     async getUser(req, res){
         const id = req.params.id
         const user = await db.query(`SELECT * FROM users WHERE id = $1`, [id])
-        res.json(user.rows)
+        res.json(user.rows ? user.rows : "nothing is here")
     }
 
+    async registration(req, res){
+
+    }
+    async login(req, res){
+
+    }
+    async check(req, res, next){
+        const {id} = req.query
+        if (!id){
+            return next(ApiError.badRequest('Не задан ID'))
+        }
+        res.json(id)
+    }
 }
 
 module.exports = new UserController()
